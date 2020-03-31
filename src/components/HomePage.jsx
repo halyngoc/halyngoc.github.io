3d4cb36d151bf03d46f209ed81650b4533fd5d31
Ha Ly <lyha@pdx.edu>
2020-03-30 19:59:37 -0700

    Change logo to logo pieces

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import '../style.css';
-import logo from '../assets/logo.svg';
+import logoP1 from '../assets/logo p1.svg';
+import logoP2 from '../assets/logo p2.svg';
+import logoP3 from '../assets/logo p3.svg';
+import logoP4 from '../assets/logo p4.svg';
 
 export default function HomePage() {
   return (
@@ -10,7 +13,10 @@ export default function HomePage() {
         <div className="jobTitle">Web developer</div>
       </div>
       <div className='logo'>
-        <img draggable="false" src={logo} alt="Logo" height='100%' width='100%'/>
+        <img className='logoP1' draggable="false" src={logoP1} alt="Logo" width='100%'/>
+        <img className='logoP2' draggable="false" src={logoP2} alt="Logo" width='100%'/>
+        <img className='logoP3' draggable="false" src={logoP3} alt="Logo" width='100%'/>
+        <img className='logoP4' draggable="false" src={logoP4} alt="Logo" width='100%'/>
       </div>
     </div>
   )
