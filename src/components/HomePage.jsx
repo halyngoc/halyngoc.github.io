7ad08a3d007059799c04ff542409b256cf904f21
Ha Ly <lyha@pdx.edu>
2020-06-17 20:04:37 -0700

    Add contact links

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import styled from 'styled-components'
 import { theme } from '../globalStyle'
 import Logo from './Logo'
+import { Icon } from '@iconify/react'
+import githubAlt from '@iconify/icons-uil/github-alt'
+import envelopeIcon from '@iconify/icons-uil/envelope'
+import phoneIcon from '@iconify/icons-uil/phone'
+import resume from '../resume.pdf'
 
 const Page = styled.section`
   overflow-x: hidden;
@@ -37,6 +42,28 @@ const Watermark = styled.div`
   transform: rotate(-10deg) scale(1.2);
 `
 
+const Contacts = styled.ul`
+  list-style: none;
+  padding: 0;
+  margin: 3rem 0 0 10%;
+
+  li {
+    margin-bottom: 1rem;
+  }
+
+  svg {
+    font-size: 2rem;
+    margin-right: 0.5rem;
+  }
+
+  a {
+    color: ${theme.text};
+    text-decoration: none;
+    display: flex;
+    align-items: center;
+  }
+`
+
 export default function HomePage() {
   return (
     <Page id="home">
@@ -45,6 +72,32 @@ export default function HomePage() {
         <p>and I'm a <Title>web developer</Title>.</p>
       </GreetingText>
       <Logo wrapper={Watermark} />
+      <Contacts>
+        <li>
+          <a href="https://github.com/halyngoc" target="_blank" rel="noopener noreferrer">
+            <Icon icon={githubAlt} />
+            github.com/halyngoc
+          </a>
+        </li>
+        <li>
+          <a href="mailto:haly.inbox@gmail.com">
+            <Icon icon={envelopeIcon} />
+            haly.inbox@gmail.com
+          </a>
+        </li>
+        <li>
+          <a href="tel:971-213-3036">
+            <Icon icon={phoneIcon} />
+            971-213-3036
+          </a>
+        </li>
+        <li>
+          <a href={resume} download="Ha_Ly_resume" target="_blank" rel="noopener noreferrer">
+            <Icon icon={githubAlt} />
+            resume (pdf, 158kb)
+          </a>
+        </li>
+      </Contacts>
     </Page>
   )
 }
