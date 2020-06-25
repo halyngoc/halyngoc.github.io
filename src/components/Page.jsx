bdb5e48270f54767e34c5fc185477a56f72c43f7
Ha Ly <lyha@pdx.edu>
2020-06-24 17:41:03 -0700

    Implement my work page

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -4,4 +4,11 @@ export const Page = styled.section`
   min-height: 100vh;
   padding: 5rem 0;
   width: 100%;
+
+  h1 {
+    font-family: Gloria Hallelujah, cursive;
+    font-weight: normal;
+    text-align: center;
+    font-size: 2rem;
+  }
 `
